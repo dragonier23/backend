6eeb2f921bf116bb4729c4e806b074e4856587aa
dragonier23 <84836280+dragonier23@users.noreply.github.com>
2023-06-13 22:20:05 +0800

    Updated getTodobyId and updatetodoById

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,13 @@ import {
     getAllTodos,
     deleteTodoById
 } from "./methods.js";
-import { updateTodoById } from "./newMethods";
+import { updateTodoById, getTodoById } from "./newMethods";
 
 const forwardRouter = Router();
 forwardRouter.post("/todos", createTodo);
 forwardRouter.get("/todos", getAllTodos);
 forwardRouter.put("/todos/:id", updateTodoById);
 forwardRouter.delete("/todos/:id", deleteTodoById);
+forwardRouter.get("/todo/:id", getTodoById);
 
 export default forwardRouter;
--- src/routes/newMethods.js
+++ src/routes/newMethods.js
@@ -10,3 +10,12 @@ export async function updateTodoById(req, res) {
     return res.status(400).json({ message: "UUID does not exist" });
   }
 }
+
+export async function getTodoById(req, res){
+  const {id} = req.params;
+  if (id in todoList){
+    return res.status(200).json(todoList[id]);
+  } else {
+    return res.status(400).json({ message: "UUID does not exist" });
+  }
+}
