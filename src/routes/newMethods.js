50040c782c2b49c1886944e109288644ca00e679
dragonier23 <84836280+dragonier23@users.noreply.github.com>
2023-06-13 22:12:25 +0800

    Update newMethods.js

--- src/routes/newMethods.js
+++ src/routes/newMethods.js
@@ -1,5 +1,12 @@
-// import { todoList } from './methods';  // Uncomment this to import todoList
+import { todoList } from './methods';  // Uncomment this to import todoList
 
 export async function updateTodoById(req, res) {
-  return res.status(501).json({ message: "Not implemented" });
+  const { id } = req.params;
+  const TodoInfo = req.body;
+  if (id in todoList){
+    todoList[id] = {...todoList[id], ...TodoInfo}
+    return res.status(200).send();
+  } else {
+    return res.status(400).json({ message: "UUID does not exist" });
+  }
 }
